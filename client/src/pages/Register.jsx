b1cfe774690fbc82486eac7aba00a54c66991c34
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-01-07 17:46:22 +0600

    Refactor Register component; enhance layout with image and improve styling for better user experience

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,9 +5,9 @@ import { FaEye, FaEyeSlash } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
 import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../provider/AuthContext";
-
+import registerImg from "../assets/register.gif";
 const Register = () => {
-    const { createUser, googleSignIn, updateUserProfile, setUser } =
+  const { createUser, googleSignIn, updateUserProfile, setUser } =
     useContext(AuthContext);
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
@@ -41,7 +41,7 @@ const Register = () => {
         updateUserProfile(name, photo).then(() => {
           setUser(result.user);
           navigate("/");
-          toast.success("Registered successfully",{
+          toast.success("Registered successfully", {
             duration: 4000,
             position: "top-center",
           });
@@ -71,98 +71,109 @@ const Register = () => {
         <meta name="og:title" content="Register - Visa Navigator" />
       </Helmet>
       <div className="min-h-screen flex items-center justify-center dark:bg-neutral sm:bg-gray-50 py-12  sm:px-6 lg:px-8">
-        <div className="max-w-lg w-full">
-          <div className="card border px-0 sm:p-6 bg-base-100 shadow-xl mx-auto">
-            <h2 className="mt-6 text-center text-3xl font-extrabold ">
-              Register
-            </h2>
-            <div className=" card-body">
-              <form onSubmit={handleRegister} className=" space-y-4">
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Name</span>
-                  </label>
-                  <input
-                    name="name"
-                    type="text"
-                    placeholder="Name"
-                    className="input input-bordered"
-                    required
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Photo URL</span>
-                  </label>
-                  <input
-                    name="photo"
-                    type="text"
-                    placeholder="photo URL"
-                    className="input input-bordered"
-                    required
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <input
-                    name="email"
-                    type="email"
-                    placeholder="email"
-                    className="input input-bordered"
-                    required
-                  />
-                </div>
-                <div className="form-control relative">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <input
-                    name="password"
-                    type={!showPassword ? "password" : "text"}
-                    placeholder="password"
-                    className="input input-bordered"
-                    required
-                  />
-                  <div className="relative w-full px-3 py-2 text-gray-400">
-                    {error && <p className="text-red-600">{error}</p>}
+        <div className="max-w-6xl w-full">
+          <div className="card grid md:grid-cols-2 border px-0 bg-base-100 shadow-xl mx-auto">
+            <div className="">
+              <div className="flex justify-center items-center h-full md:border-r-4 ">
+                <img
+                  src={registerImg}
+                  alt="register"
+                  className="rounded-xl h-48 md:h-auto object-cover"
+                />
+              </div>
+            </div>
+            <div className="sm:p-6 ">
+              <h2 className="mt-6 text-center text-3xl font-extrabold ">
+                Register
+              </h2>
+              <div className=" card-body">
+                <form onSubmit={handleRegister} className=" space-y-4">
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Name</span>
+                    </label>
+                    <input
+                      name="name"
+                      type="text"
+                      placeholder="Name"
+                      className="input input-bordered"
+                      required
+                    />
                   </div>
-                  {showPassword ? (
-                    <FaEye
-                      size={20}
-                      className=" cursor-pointer absolute right-5 translate-y-12 top-1 "
-                      onClick={() => setShowPassword(!showPassword)}
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Photo URL</span>
+                    </label>
+                    <input
+                      name="photo"
+                      type="text"
+                      placeholder="photo URL"
+                      className="input input-bordered"
+                      required
                     />
-                  ) : (
-                    <FaEyeSlash
-                      size={20}
-                      className=" cursor-pointer absolute right-5 translate-y-12 top-1 "
-                      onClick={() => setShowPassword(!showPassword)}
+                  </div>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input
+                      name="email"
+                      type="email"
+                      placeholder="email"
+                      className="input input-bordered"
+                      required
                     />
-                  )}
-                </div>
-                <div className="form-control my-3">
-                  <button type="submit" className="btn btn-neutral">
-                    Register
+                  </div>
+                  <div className="form-control relative">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <input
+                      name="password"
+                      type={!showPassword ? "password" : "text"}
+                      placeholder="password"
+                      className="input input-bordered"
+                      required
+                    />
+                    <div className="relative w-full px-3 py-2 text-gray-400">
+                      {error && <p className="text-red-600">{error}</p>}
+                    </div>
+                    {showPassword ? (
+                      <FaEye
+                        size={20}
+                        className=" cursor-pointer absolute right-5 translate-y-12 top-1 "
+                        onClick={() => setShowPassword(!showPassword)}
+                      />
+                    ) : (
+                      <FaEyeSlash
+                        size={20}
+                        className=" cursor-pointer absolute right-5 translate-y-12 top-1 "
+                        onClick={() => setShowPassword(!showPassword)}
+                      />
+                    )}
+                  </div>
+                  <div className="form-control my-3">
+                    <button type="submit" className="btn btn-neutral">
+                      Register
+                    </button>
+                  </div>
+                  <p className="text-sm text-center">
+                    Already have an account?{" "}
+                    <Link to="/login" className="font-bold underline">
+                      Login here
+                    </Link>
+                  </p>
+                </form>
+                <div className="divider">OR</div>
+                <div className="">
+                  <button
+                    onClick={handleGoogleSignIn}
+                    className="btn shadow w-full"
+                  >
+                    <FcGoogle className="text-xl" />
+                    <span className="">Register in with Google</span>
                   </button>
                 </div>
-                <p className="text-sm text-center">
-                  Already have an account?{" "}
-                  <Link to="/login" className="font-bold underline">
-                    Login here
-                  </Link>
-                </p>
-              </form>
-              <div className="divider">OR</div>
-              <div className="">
-                <button
-                  onClick={handleGoogleSignIn}
-                  className="btn shadow w-full"
-                >
-                  <FcGoogle className="text-xl" />
-                  <span className="">Register in with Google</span>
-                </button>
               </div>
             </div>
           </div>
@@ -170,6 +181,6 @@ const Register = () => {
       </div>
     </>
   );
-}
+};
 
-export default Register
+export default Register;
